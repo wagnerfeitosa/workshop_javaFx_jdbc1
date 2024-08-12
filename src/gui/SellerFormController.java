473425bc098edfd6ea0372e9511821028c5fad0b
wagner feitosa <wagnerfeitosa2000@hotmail.com>
2024-08-12 13:10:20 -0300

    Seller Form

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,150 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.servicies.SellerService;
+/*Essa classe é Observer é Subject ou seja a classe que Emite o evento */
+public class SellerFormController implements Initializable{
+	
+	private Seller entity;
+	
+	private SellerService service;
+	//Lista de objetos escritos interessados em receber eventos 
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErroName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	//injeção de independencia
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	//Resposavel em escrever listener na lista
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			//metodo de notificação
+			notifyDataChangeListeners();
+			
+			//Apos salvar os dado fechar a jenela
+			Utils.currentsStage(event).close();;
+			
+		}catch(DbException e) {
+			Alerts.showAlerts("Error Saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErros());
+		}
+		
+	}
+	//Responsavel por percorrer a lista e notificando 
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.ondaDataChanged();
+			
+		}
+		
+	}
+	/*Resposavel por pegar os dados do digitados no TextField
+	 * e adicionar no objeto Seller */
+	private Seller getFormData() {
+		
+		Seller obj = new Seller();
+		ValidationException exception = new ValidationException("Validation Error");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		  //trim elimina qualque espaco em branco que esteja no inicio ou na final
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			//adicionando erro
+			exception.addErros("name", "o campo não pode ser vazio");
+		}
+		obj.setName(txtName.getText());
+		//se ouver um erro é lançado a exeption
+		if(exception.getErros().size() > 0) {
+			throw exception;
+		}
+		
+		return obj;
+	}
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentsStage(event).close();;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializaNode();
+		
+	}
+	//Metodo de restrições
+	public void initializaNode() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 20);
+		
+	}
+	//Metodo Responsavel por pegar uma entity e popular caixa de texto do formulario
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		//String.valueOf converte para string
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	//responsavel por pegar os erros da exception e escrever os erros na tela
+	private void setErrorMessages(Map<String,String> erros) {
+		
+		Set<String> fields = erros.keySet();
+		
+		if(fields.contains("name")) {
+			labelErroName.setText(erros.get("name"));
+		}
+	}
+
+}
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.servicies.SellerService;
@@ -122,7 +127,7 @@ public class SellerListController implements Initializable,DataChangeListener{
 	 * Recebe como argumento um fxml e um Stage no caso o evento do botao*/
 	private void createDialogForm(Seller obj,String absoluteName, Stage parentStage) {
 		
-		/*try {
+		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
 			
@@ -153,7 +158,7 @@ public class SellerListController implements Initializable,DataChangeListener{
 		}
 		catch(IOException e) {
 			Alerts.showAlerts("IO Exception", "Error loading View ", e.getMessage(), AlertType.ERROR);
-		}*/
+		}
 	}
 	
 	//Resposavel por executar a notificação quando os dados forem alterados
